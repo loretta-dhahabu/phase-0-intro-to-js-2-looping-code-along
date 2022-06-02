20731581a15b13cdbd757163068a7b528d7c42c3
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-02 18:20:15 +0300

    commit tree 9458d74ffeeb489ce62954b66072387f7eb0b1a2

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const name = [ "Guadalupe", "Ollie", "Aki" ];
+const event = "surprise";
+const gifts = [(["Guadalupe", "Ollie", "Aki"], "surprise")];
+function writeCards ( names, event )
+{
+    for ( let i = 0; i < names.length; i++ )
+    {
+        gifts[ i ] = ( `Thank you, ${names[ i ]}, for the wonderful ${ event } gift!` );
+        console.log( gifts[ i ] );
+        debugger;
+    }
+    return gifts;
+}
+writeCards( names, event );
+
+const number = [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ]; 
+function countDown (number)
+{
+    let i = number;
+    while ( i >= 0 )
+    {
+        console.log( i );
+        i-=1;
+    }
+}
+countDown( 10 );
+
+
